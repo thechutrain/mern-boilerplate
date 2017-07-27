f382617ed9c89503e394c42b702a54e8d308aadb
thechutrain <chu.chuyalan@gmail.com>
2017-07-27 11:03:57 -0400

    added bodyparser and morgan to log http requests to my server

--- server/server.js
+++ server/server.js
@@ -1,7 +1,15 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
 const app = express()
 const PORT = process.env.PORT || 8080
 
+app.use(morgan('dev'))
+app.use(
+	bodyParser.urlencoded({
+		extended: false
+	})
+)
 // Test that my server is working
 app.get('/api/data', (req, res) => {
 	res.json({ data: ['a', 'b', 'c'] })
