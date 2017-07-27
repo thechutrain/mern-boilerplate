5789ec5ed7875f22d1774d14769be1f4d084becf
thechutrain <chu.chuyalan@gmail.com>
2017-07-27 11:12:54 -0400

    added production and dev connection to my mongo db

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
+const mongoose = require('mongoose')
 const app = express()
 const PORT = process.env.PORT || 8080
 
@@ -10,11 +11,21 @@ app.use(
 		extended: false
 	})
 )
+
+// ==== Connecting to the Mongo DataBase ====
+if (process.env.MONGODB_URI) {
+	mongoose.connect(process.env.MONGODB_URI)
+} else {
+	// local mongo url
+	mongoose.connect('mongodb://localhost/mern-boilerplate')
+}
+// ==== ROUTES =====
 // Test that my server is working
 app.get('/api/data', (req, res) => {
 	res.json({ data: ['a', 'b', 'c'] })
 })
 
+// ==== Starting Server =====
 app.listen(PORT, () => {
 	console.log(`App listening on PORT: ${PORT}`)
 })
